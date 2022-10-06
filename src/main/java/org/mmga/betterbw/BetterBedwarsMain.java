d2211d91b207cd9878ff28a92f0e45fea5c806db
Wzp-2008 <w433200w>
2022-10-06 22:25:30 +0800

    Initial commit

---
+++ src/main/java/org/mmga/betterbw/BetterBedwarsMain.java
@@ -0,0 +1,23 @@
+package org.mmga.betterbw;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created On 2022/10/6 22:19:35
+ *
+ * @author wzp
+ * @version 1.0.0
+ */
+public final class BetterBedwarsMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
